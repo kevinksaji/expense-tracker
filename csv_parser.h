219b22562e2453c18186340f5bc13f6bdc6c0e24
kevinksaji <kevink.saji.2022@scis.smu.edu.sg>
2026-04-17 23:04:17 +0800

    add CSV parser implementation with transaction structure

---
+++ csv_parser.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct Transaction
+{
+    std::string type;
+    std::string started_date;
+    std::string completed_date;
+    std::string description;
+    double amount;
+    double fee;
+    std::string currency;
+    std::string state;
+    double balance;
+    std::string category;
+};
